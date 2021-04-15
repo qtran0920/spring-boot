e9ab3a580b6f12072682347c832f9fc34dd2e4f0
Jimmy Tran <qtran11@gmu.edu>
2021-04-15 15:39:18 -0400

    refactor(MathUtilTest): address sonarqube issue

--- src/test/java/com/example/springboot/util/MathUtilTest.java
+++ src/test/java/com/example/springboot/util/MathUtilTest.java
@@ -2,31 +2,31 @@ package com.example.springboot.util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MathUtilTest {
+class MathUtilTest {
 
     // https://www.mathsisfun.com/numbers/fibonacci-sequence.html
     // http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibtable.html
 
     @Test
-    public void fibonacci_0() {
-       assertEquals(0,  MathUtil.fibonacci(0));
+    void fibonacci_0() {
+        assertEquals(0, MathUtil.fibonacci(0));
     }
 
     @Test
-    public void fibonacci_1() {
-        assertEquals(1,  MathUtil.fibonacci(1));
+    void fibonacci_1() {
+        assertEquals(1, MathUtil.fibonacci(1));
     }
 
     @Test
-    public void fibonacci_2() {
-        assertEquals(1,  MathUtil.fibonacci(2));
+    void fibonacci_2() {
+        assertEquals(1, MathUtil.fibonacci(2));
     }
 
     @Test
-    public void fibonacci_3() {
-        assertEquals(2,  MathUtil.fibonacci(3));
+    void fibonacci_3() {
+        assertEquals(2, MathUtil.fibonacci(3));
     }
 
 }
